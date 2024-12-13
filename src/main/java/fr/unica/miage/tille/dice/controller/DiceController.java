19706589e45bbf296ca848a8f472ac8202182979
LogannTille <logann.tille@etu.unice.fr>
2024-12-13 10:02:05 +0100

    bonus fonctionnel

--- src/main/java/fr/unica/miage/tille/dice/controller/DiceController.java
+++ src/main/java/fr/unica/miage/tille/dice/controller/DiceController.java
@@ -1,21 +1,49 @@
 package fr.unica.miage.tille.dice.controller;
 
-import fr.unica.miage.tille.dice.service.DiceService;
+import fr.unica.miage.tille.dice.entity.DiceRollLog;
+import fr.unica.miage.tille.dice.entity.DiceRollLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import fr.unica.miage.tille.dice.service.Dice;
 
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class DiceController {
 
     @Autowired
-    private DiceService diceService;
+    private Dice dice;
+
+    @Autowired
+    private DiceRollLogRepository diceRollLogRepository;
+
+    @GetMapping("/rollDice")
+    public int rollDice() {
+        int result = dice.roll();
+        saveDiceRollLog(1, List.of(result));
+        return result;
+    }
 
     @GetMapping("/rollDices/{X}")
     public List<Integer> rollDices(@PathVariable int X) {
-        return diceService.rollDices(X);
+        List<Integer> results = new ArrayList<>();
+        for (int i = 0; i < X; i++) {
+            results.add(dice.roll());
+        }
+        saveDiceRollLog(X, results);
+        return results;
+    }
+
+    private void saveDiceRollLog(int diceCount, List<Integer> results) {
+        DiceRollLog log = new DiceRollLog();
+        log.setDiceCount(diceCount);
+        log.setResults(results);
+        log.setTimestamp(LocalDateTime.now());
+        diceRollLogRepository.save(log);
     }
 }
--- src/main/java/fr/unica/miage/tille/dice/entity/DiceRollLog.java
+++ src/main/java/fr/unica/miage/tille/dice/entity/DiceRollLog.java
@@ -1,10 +1,12 @@
 package fr.unica.miage.tille.dice.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Data
 public class DiceRollLog {
 
     @Id
@@ -14,14 +16,4 @@ public class DiceRollLog {
     @ElementCollection
     private List<Integer> results;
     private LocalDateTime timestamp;
-
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public int getDiceCount() { return diceCount; }
-    public void setDiceCount(int diceCount) { this.diceCount = diceCount; }
-    public List<Integer> getResults() { return results; }
-    public void setResults(List<Integer> results) { this.results = results; }
-    public LocalDateTime getTimestamp() { return timestamp; }
-    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
 }
